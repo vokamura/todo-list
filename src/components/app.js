eab2e229376659b5bdc7895e6460b8d01e147b33
vokamura <vcokamura@gmail.com>
2018-06-26 12:18:11 -0700

    Get data from server and add

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,8 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React, {Component} from 'react';
 import AddItem from './add_item';
 import List from './list';
-import listData from '../data/list';
+// import listData from '../data/list';
+import axios from 'axios';
 
 class App extends Component {
     constructor(props){
@@ -10,23 +11,58 @@ class App extends Component {
         this.state = {
             list: []
         };
+        this.base_url = 'http://api.reactprototypes.com';
+        this.api_key = '?key=c418keykey';
     }
 
     componentDidMount(){
         this.getListData();
     }
 
-    addItem(item){
-        this.setState({
-            list: [item, ...this.state.list]
-        });
+    // addItem(item){
+        // this.setState({
+        //     list: [item, ...this.state.list]
+        // });
+
+    // }
+
+    async addItem(item){ 
+       try {
+            await axios.post(`${this.base_url}/todos${this.api_key}`, item);
+            this.getListData();
+       } catch(err) {
+            console.log('Error adding item: ', err.response.data);
+       }
     }
 
-    getListData(){
+    // getListData(){
+        //Using dummy data
+        // this.setState({
+        //     list: listData
+        // });
+
         //Make call to server to get data
-        this.setState({
-            list: listData
-        });
+        // axios.get(`${this.base_url}/todos${this.api_key}`).then(resp => {
+        //     console.log('Get Todos Response: ', resp.data.todos);
+        //     this.setState({
+        //         list: resp.data.todos
+        //     })
+        // }).catch(err => {
+        //     console.log('Get todos error: ', err.message);
+        // });
+    // }
+
+    //Async await.  Have to include async and await words.  Biggest benefit makes code looks like it's executing in a line
+    async getListData() {
+        //try catch block
+        try {
+            const resp = await axios.get(`${this.base_url}/todos${this.api_key}`);
+            this.setState({
+                list: resp.data.todos
+            });
+        } catch (err) {
+            console.log('Get Data Error:', err.message);
+        }
     }
 
     render(){
